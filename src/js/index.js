2ffa75d526fe8a303785958fceffc37170b0a243
bocacrst <61462231+bocacrst@users.noreply.github.com>
2020-04-12 14:17:30 +0300

    kfl

--- src/js/index.js
+++ src/js/index.js
@@ -127,3 +127,29 @@ $(document).ready(function () {
 
     })
 })
+
+/*#####################################################*/
+/*-------------------CONTACT-FORM----------------------*/
+/*#####################################################*/
+
+$(document).ready(function(){
+    $('.js--section-slider').waypoint(function(direction){
+        if(direction == 'down'){
+            $('.nav').addClass('sticky');
+            
+        }else{
+            $('.nav').removeClass('sticky');
+        }
+
+    });
+    
+});
+/*
+
+var waypoints = $('#handler-first').waypoint(function(direction) {
+  notify(this.element.id + ' hit 25% from top of window') 
+}, {
+  offset: '25%'
+})
+
+*/
